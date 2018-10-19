8b77f4e7604e6f7115ecbd0806332787eb892d08
Harrison <harrisonnguyen@students.berkeley.net>
2018-10-19 12:59:14 -0700

    hi

--- src/zoo.java
+++ src/zoo.java
@@ -1,20 +1,42 @@
 public class zoo {
      String favoriteFood = "bacon";
+
     String newLine = System.getProperty("line.separator");
 
     public static void main(String[] args){
-        zoo t = new zoo();
+        /*zoo t = new zoo();
         t.sleep("Tigger");
 
         zoo x = new zoo();
         x.eat("Tigger", "meat");
         x.eat("Tigger", "bacon");
+        */
 
         Tiger tigger = new Tiger("Tigger");
         tigger.eat("meat");
-        tigger.eat("kibble");
-        tigger.sleep("Tigger");
+
+        Bear pooh = new Bear("Pooh");
+        pooh.eat("fish");
+        pooh.eat("meat");
+
+        Unicorn rarity = new Unicorn("Rarity");
+        rarity.eat("marshmallow");
+
+        Giraffe gemma = new Giraffe("Gemma");
+        gemma.eat("meat");
+        gemma.eat("leaves");
+
+        Bee stinger = new Bee("Stinger");
+        stinger.eat("ice cream");
+        stinger.eat("pollen");
+
+        Animal[] animalList = {tigger,pooh,rarity,gemma,stinger};
+
+        Zookeeper zoebot = new Zookeeper("Zoebot");
+        zoebot.feedAnimals(animalList,"food");
+
     }
+
 
     public void sleep(String name){
         System.out.print(name + " sleeps for 8 hours"+newLine);
